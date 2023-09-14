c5b5b5971d9374055ac46525d8634bae82115a21
Stella <90323575+stellaric@users.noreply.github.com>
2023-09-14 15:22:14 +0200

    Create Accueil.js dans le dossier accueil

---
+++ frontend_vop/vop/src/Pages/Accueil/Accueil.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import React from 'react';
+
+export default function Accueil() {
+    return (
+       
+        <>
+     <Header/>
+
+     </>
+    );
+}
+
+ 
