21e9a9ca5b1391a20efc583d983c02873ce42995
Jade-dev-28 <133015219+Jade-dev-28@users.noreply.github.com>
2023-09-14 15:50:03 +0200

    test

--- frontend_vop/vop/src/App.js
+++ frontend_vop/vop/src/App.js
@@ -1,18 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import Accueil from './Pages/Accueil/Accueil';
-
+import './App.css';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Accueil />} />
-        </Routes>
-      </Router>
+      
+        <Router>
+          <Routes>
+
+            <Route path="/" element={<Accueil />} />
+
+          </Routes>
+        </Router>
+      
+
     </div>
   );
 }
